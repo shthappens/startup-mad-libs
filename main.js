4f15f71223094e1732a54df50df2561146feed3b
Seth Thompson <seth.h.thompson@gmail.com>
2017-04-29 09:21:02 -0400

    Completed exercise

---
+++ main.js
@@ -0,0 +1,18 @@
+let companyName = 'SHTHappens, Inc.';
+let anOffering = 'a revolutionary new platform';
+let aTargetAudience = 'the universe';
+let solveAProblem = 'create new businesses for dynamic markets';
+let secretSauce = 'by building invite-only Internets';
+
+// your code, here
+let firstLine = 'My company, '+ companyName +',';
+let secondLine = 'is developing ' + anOffering;
+let thirdLine = 'to help ' + aTargetAudience;
+let fourthLine = solveAProblem;
+let fifthLine = secretSauce + '.';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
+console.log(fourthLine);
+console.log(fifthLine);
