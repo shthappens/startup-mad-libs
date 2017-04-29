be3558334214ce4fec0dd601707372d531ba9eef
Seth Thompson <seth.h.thompson@gmail.com>
2017-04-29 09:40:40 -0400

    small refactoring

--- main.js
+++ main.js
@@ -5,14 +5,7 @@ let solveAProblem = 'create new businesses for dynamic markets';
 let secretSauce = 'by building invite-only Internets';
 
 // your code, here
-let firstLine = 'My company, '+ companyName +',';
-let secondLine = 'is developing ' + anOffering;
-let thirdLine = 'to help ' + aTargetAudience;
-let fourthLine = solveAProblem;
-let fifthLine = secretSauce + '.';
+let pitch = document.getElementById('main');
+pitch.innerHTML = 'My company, '+ companyName +',\nis developing ' + anOffering + '\nto help ' + aTargetAudience +'\n' + solveAProblem + '\n' + secretSauce + '.';
 
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
-console.log(fourthLine);
-console.log(fifthLine);
+console.log(pitch);
